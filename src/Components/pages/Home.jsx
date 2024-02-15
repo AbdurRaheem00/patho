e771cfed13291045c4f20334b631f9ac78137b1c
Abdur Raheem <134161216+AbdurRaheem00@users.noreply.github.com>
2024-02-16 00:46:29 +0530

    original

--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -33,7 +33,7 @@ const Home = () => {
       <div className="bg-gradient-to-r from-black to-gray-900 sm:w-50 md: w-100 ">
         <div className="homecontainer relative">
           <img src="guy.png" alt="" className=" hidden sm:block png absolute w-[800px] right-4 "/>
-          <h1 className="text1 md:text-7xl lg:text-8xl xl:text-6xl 2xl:text-8xl font-bold text-[35px]">
+          <h1 className="text1 md:text-7xl lg:text-8xl xl:text-6xl 2xl:text-8xl font-bold text-[35px] text-[]">
             Garuda.Metaverse
           </h1>
           <h1 className="text2 md:text-3xl text-left  font-normal text-balance text-[16px]">
